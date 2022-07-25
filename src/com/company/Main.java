8ca049416d47289e67a939046fedd4081f27f90e
nbor <41956868+nadyamborisova@users.noreply.github.com>
2022-07-25 20:54:42 +0300

    my changes are the best]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Initial code here...");
+        System.out.println("Initial code here... User2 Changes");
     }
 }
